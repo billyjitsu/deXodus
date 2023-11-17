b68921a62fe7a7f9698da5c01b989fd8a9d7f7f2
0xJalon <0xjalon@0xJalons-MacBook-Pro.local>
2023-11-17 23:18:48 +0300

    testing first push

--- src/Counter.sol
+++ src/Counter.sol
@@ -5,7 +5,7 @@ contract Counter {
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
-        number = newNumber;
+        number = newNumber + 1;
     }
 
     function increment() public {
